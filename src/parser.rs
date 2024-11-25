2c66513d9d6a20139fb588b40e9d46e5c774fe67
Itron_Al_Lenn <itronallenn@gmail.com>
2024-11-25 01:09:03 +0100

    feat: Added a new parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,13 @@
 use crate::input::PuzzleInput;
 
 pub fn by_line(input: &PuzzleInput) -> Vec<String> {
+    input.0.lines().map(String::from).collect()
+}
+
+pub fn by_line_and_empty_line(input: &PuzzleInput) -> Vec<Vec<String>> {
     input
         .0
-        .split("\n")
-        .map(|x| -> String { String::from(x) })
+        .split("\n\n")
+        .map(|lines| lines.lines().map(String::from).collect())
         .collect()
 }
